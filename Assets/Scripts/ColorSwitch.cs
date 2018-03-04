2c45a14677c7cfd65d4184d6b58d28482b0cb71d
Loiso <bogdanrybak71@gmail.com>
2018-03-04 23:48:58 +0200

    Normal level 2

---
+++ Assets/Scripts/ColorSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwitch : MonoBehaviour {
+
+	Material m_Material;
+
+	void Start () 
+	{
+		m_Material = GetComponent<Renderer>().material;
+		m_Material.color = Color.magenta;
+	}
+
+}
